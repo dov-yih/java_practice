cd84b4e05c29629e5cd3874848c6cffd07a1bdbe
dafuyi <1427019390@qq.com>
2017-03-27 08:16:43 +0800

    add the getter and setter function

--- User.java
+++ User.java
@@ -6,12 +6,36 @@ public class User{
   public User(){
 
   }
-
   public User(String name,String passwd, String email){
     this.name = name;
     this.passwd = passwd;
     this.email = email;
   }
+
+  public void setname(String name) {
+      this.name = name;
+  }
+
+  public void setPasswd(String passwd) {
+    this.passwd = passwd;
+  }
+
+  public void SetEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName(){
+      return this.name;
+  }
+
+  public String getPasswd() {
+      return this.passwd;
+  }
+
+  public String getEmail() {
+      return this.email;
+  }
+  
   public void print(){
     System.out.println(this.name + "  " + this.passwd + "  " + this.email);
   }
